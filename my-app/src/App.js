8c61612604ffd733ecd5e1bb95d3354ffe50f21b
Balja <balja@Uchkis-MacBook-Pro.local>
2023-02-10 22:43:29 +0800

    q

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,22 +8,25 @@ import Login from "./components/subcomponents/Login";
 
 import User from "./components/subcomponents/User";
 
+import { UserContext } from "./layout/UserContext";
 import { ProductContext } from "./layout/ProductContext";
 import { useState } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
-
+  const [users, setUsers] = useState([]);
   return (
     <div className="App">
-      <Header />
-      <ProductContext.Provider value={{ products, setProducts }}>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/user" element={<User />} />
-        </Routes>
-      </ProductContext.Provider>
+      <UserContext.Provider value={{ users, setUsers }}>
+        <Header />
+        <ProductContext.Provider value={{ products, setProducts }}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/user" element={<User />} />
+          </Routes>
+        </ProductContext.Provider>
+      </UserContext.Provider>
 
       <Footer />
     </div>
--- my-app/src/components/subcomponents/Login.jsx
+++ my-app/src/components/subcomponents/Login.jsx
@@ -2,11 +2,12 @@ import "../styles/login.css";
 import { useContext } from "react";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDataContext } from "../../layout/DataContext";
+import { UserContext } from "../../layout/UserContext";
+// import { useDataContext } from "../../layout/DataContext";
 import axios from "axios";
 import { useEffect } from "react";
 export default function Login() {
-  const { users, setUsers } = useDataContext();
+  const { users, setUsers } = useContext(UserContext);
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [isLogged, setIsLogged] = useState(false);
@@ -24,36 +25,35 @@ export default function Login() {
     console.log("email is" + " " + e.target.email.value);
     console.log("password is" + " " + e.target.password.value);
     console.log(users);
-
-    const check = users.map((user) => {
-      if (user.email === email && user.password === pass) {
-        setIsLogged(true);
-        navigate("/");
-      }
-    });
-
-    return (
-      <div className="login">
-        <form onSubmit={handleSubmit}>
-          <label for="email">Email</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            id="email"
-            name="email"
-            type="email"
-            placeholder="enter your email"
-          ></input>
-          <label for="password">Password</label>
-          <input
-            onChange={(e) => setPass(e.target.value)}
-            id="password"
-            name="password"
-            type="password"
-            placeholder="enter your password"
-          ></input>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    );
   };
+  const check = users.map((user) => {
+    if (user.email === email && user.password === pass) {
+      setIsLogged(true);
+      navigate("/");
+    }
+  });
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <label for="email">Email</label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          name="email"
+          type="email"
+          placeholder="enter your email"
+        ></input>
+        <label for="password">Password</label>
+        <input
+          onChange={(e) => setPass(e.target.value)}
+          id="password"
+          name="password"
+          type="password"
+          placeholder="enter your password"
+        ></input>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
